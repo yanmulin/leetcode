c9b1dff0ebb77ffef04cb00956bf3b0c91569ef0
mulin <yans1996@outlook.com>
2020-01-07 02:05:46 +0800

    update 11.container-with-most-water.cpp

--- 11.container-with-most-water.cpp
+++ 11.container-with-most-water.cpp
@@ -8,18 +8,16 @@ private:
     }
 public:
     int maxArea(vector<int>& height) {
-        int size = height.size();
-        if (size < 2) return 0;
-        int max = computeArea(height, 0, 1);
-        int i = 0, j = 1;
-        for (int k=2;k<size;k++) {
-            for (int n=0;n<=k;n++) {
-                int area = computeArea(height, n, k);
-                if (area >= max) {
-                    i = n; j = k;
-                    max = area;
-                }
+        int i = 0, j = height.size() - 1;
+        int max = computeArea(height, i, j);
+        while (i < j) {
+            if (height[i] < height[j]) {
+                i ++;
+            } else {
+                j --;
             }
+            int area = computeArea(height, i, j);
+            max = MAX(area, max);
         }
         return max;
     }
