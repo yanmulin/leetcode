b9c34b59f2aed4c3c7b6fea98f3098980e02ea00
mulin <yans1996@outlook.com>
2020-01-07 15:35:00 +0800

    update 20.valid-parentheses.cpp

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool match(char l, char r) {
+        if (l == '(' && r == ')') return true;
+        if (l == '{' && r == '}') return true;
+        if (l == '[' && r == ']') return true;
+        return false;
+    }
+public:
+    bool isValid(string s) {
+        int size = s.size();
+        stack<char> stk;
+        for (int i=0;i<size;i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stk.push(s[i]);
+            } else if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+                if (stk.size() == 0) return false;
+                char c = stk.top();
+                stk.pop();
+                if (!match(c, s[i])) return false;
+            }
+        }
+        if (stk.size() > 0) return false;
+        return true;
+    }
+};
