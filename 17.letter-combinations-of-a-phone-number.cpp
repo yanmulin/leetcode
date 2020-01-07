c4359f26774a9d10794d682bf8f6a00d32829616
mulin <yans1996@outlook.com>
2020-01-07 13:07:59 +0800

    update 17.letter-combinations-of-a-phone-number.cpp

--- 17.letter-combinations-of-a-phone-number.cpp
+++ 17.letter-combinations-of-a-phone-number.cpp
@@ -3,30 +3,19 @@
 #include <vector>
 using namespace std;
 class Solution {
-private:
-    string mapping(char digit) {
-        switch (digit) {
-            case '2': return "abc";
-            case '3': return "def";
-            case '4': return "ghi";
-            case '5': return "jkl";
-            case '6': return "mno";
-            case '7': return "pqrs";
-            case '8': return "tuv";
-            case '9': return "wxyz";
-            default: assert(false);
-        }
-    }
 public:
     vector<string> letterCombinations(string digits) {
+        vector<string> mapping = {"abc", "def", 
+                                    "ghi", "jkl", "mno", 
+                                    "pqrs", "tuv", "wxyz"};
         int size = digits.size();
         if (size == 0) return {};
         vector<string> res;
-        string s = mapping(digits[0]);
+        string s = mapping[digits[0] - '2'];
         for (int k=0;k<s.size();k++)
                 res.push_back({s[k]});
         for (int i=1;i<size;i++) {
-            string s = mapping(digits[i]);
+            string s = mapping[digits[i] - '2'];
             vector<string> copy = res;
             res.clear();
             for (int j=0;j<copy.size();j++) {
