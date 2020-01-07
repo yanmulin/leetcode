619f85fd3e6709a5d9c59b72083c2f3eb3592091
mulin <yans1996@outlook.com>
2020-01-07 13:05:15 +0800

    update 17.letter-combinations-of-a-phone-number.cpp

--- 17.letter-combinations-of-a-phone-number.cpp
+++ 17.letter-combinations-of-a-phone-number.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 class Solution {
 private:
-    vector<string> mapping(char digit) {
+    string mapping(char digit) {
         switch (digit) {
-            case '2': return {"a", "b", "c"};
-            case '3': return {"d", "e", "f"};
-            case '4': return {"g", "h", "i"};
-            case '5': return {"j", "k", "l"};
-            case '6': return {"m", "n", "o"};
-            case '7': return {"p", "q", "r", "s"};
-            case '8': return {"t", "u", "v"};
-            case '9': return {"w", "x", "y", "z"};
+            case '2': return "abc";
+            case '3': return "def";
+            case '4': return "ghi";
+            case '5': return "jkl";
+            case '6': return "mno";
+            case '7': return "pqrs";
+            case '8': return "tuv";
+            case '9': return "wxyz";
             default: assert(false);
         }
     }
@@ -21,22 +21,19 @@ public:
     vector<string> letterCombinations(string digits) {
         int size = digits.size();
         if (size == 0) return {};
-        vector<vector<string>> p;
-        for (int i=size-1;i>=0;i--) p.push_back(mapping(digits[i]));
-        while (p.size() > 1) {
-            vector<string> v, v1, v2;
-            v1 = p.back();
-            p.pop_back();
-            v2 = p.back();
-            p.pop_back();
-            int v1sz = v1.size(), v2sz = v2.size();
-            for (int j=0;j<v1sz;j++) {
-                for (int k=0;k<v2sz;k++) {
-                    v.push_back(v1[j] + v2[k]);
-                }
+        vector<string> res;
+        string s = mapping(digits[0]);
+        for (int k=0;k<s.size();k++)
+                res.push_back({s[k]});
+        for (int i=1;i<size;i++) {
+            string s = mapping(digits[i]);
+            vector<string> copy = res;
+            res.clear();
+            for (int j=0;j<copy.size();j++) {
+                for (int k=0;k<s.size();k++)
+                    res.push_back(copy[j] + s[k]);
             }
-            p.push_back(v);
         }
-        return p.back();
+        return res;
     }
 };
