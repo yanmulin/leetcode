1afab045ebaa959ace7c0e68b4fd3581517634b6
mulin <yans1996@outlook.com>
2020-01-07 15:37:06 +0800

    update 20.valid-parentheses.cpp

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -9,6 +9,8 @@ private:
 public:
     bool isValid(string s) {
         int size = s.size();
+        if (size == 0) return true;
+        if (size % 2) return false;
         stack<char> stk;
         for (int i=0;i<size;i++) {
             if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
