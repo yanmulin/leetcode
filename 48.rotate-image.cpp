fcdf3e6c642d50c47210184f933c96e86cf609b3
mulin <yans1996@outlook.com>
2020-01-12 20:54:00 +0800

    update 48.rotate-image.cpp

---
+++ 48.rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i=0;i<n/2;i++) {
+            int low = i, high = n - i - 1;
+            for (int j=low;j<high;j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+};
